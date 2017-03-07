fd35cf61ff514fe5af22bfac4984db30c04e602e
chetanrbb <bornarkar.chetan@gmail.com>
2017-03-07 01:00:22 -0800

    R

--- project_CPUTesting_Module/project_CPUTesting_Module.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.4\\bin
-call %xv_path%/xelab  -wto cac8322e6dbf47de92a0fa031943d143 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot testbench_cpu_behav xil_defaultlib.testbench_cpu xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
